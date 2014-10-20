5c40159767d956b285321dc95fd29bb422b42071
Benjamin Parker <benjaminparker@gmail.com>
2014-10-20 14:29:42 -0500

    functional

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :authors
 
   resources :articles
-  root to: "articles#index"
-  
+  root to: "articles#show", id: 1
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
